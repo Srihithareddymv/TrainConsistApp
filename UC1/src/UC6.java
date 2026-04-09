595c423e7c8cc4604ee29b261040f1b7eb569b8c
Srihithareddymv <mvsrihitha13@gmail.com>
2026-04-09 09:38:29 +0530

    UC6 Commited

---
+++ UC1/src/UC6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class UC6 {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("Sleeper", 72);
+        map.put("AC Chair", 56);
+        map.put("First Class", 24);
+
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            System.out.println(e.getKey() + " -> " + e.getValue());
+        }
+    }
+}
